d841a283f5e9ae29441120a3819c8128fa1a45f4
Tatiana B <tbeketova04@gmail.com>
2024-03-05 14:11:52 +0200

    добавляет работающий код слайдера

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-//showSlides функция
-function showSlides(sliderNum, n) {
-  let slides = document.querySelectorAll(`.slider-wrapper[data-slider="${sliderNum}"] .slider__item`);
-  let dots = document.querySelectorAll(`.slider-wrapper[data-slider="${sliderNum}"] .dot`);
-  
-  slides = Array.from(slides);  //Преобразование коллекций в массивы
-  dots = Array.from(dots); 
+//Функция инициализации слайдера
+function initSlider() {
+	let slides = document.querySelectorAll(".slider__wrapper  .slider__item");
+	let dots = document.querySelectorAll(".slider__wrapper  .dot");
 
-  slides.forEach(slider__item => {    // Скрываем все слайды
-    slider__item.style.display = 'none';
-  });
+	slides = Array.from(slides); //Преобразование выбранных элементов слайдов и точек в массивы
+	dots = Array.from(dots);
 
-  dots.forEach(dot => {  //Удаляем класс 'active' у всех точек
-    dot.classList.remove('active');
-  });
+// Функция для сброса активных классов и стилей слайдов и кнопок-пагинации
+	function slidesResetActive() {
 
-  slides[n].style.display = 'block'; // Отображаем выбранный слайд
-  dots[n].classList.add('active'); //Добавляем класс 'active' выбранной точке
+		//вызов метода forEach, который выполняет указанную функцию один раз для каждого элемента в массиве slides. Каждый элемент массива передается в функцию как аргумент, и также передается индекс этого элемента в массиве.
+		slides.forEach((slide, index) => { //Для каждого слайда и его индекса. slide - текущий элемент массива slides, который обрабатывается в текущей итерации. index- ето индекс текущего элемента slide в массиве slides
+			if (slide.classList.contains("active")) {
+				slide.classList.remove("active");
+				slide.style.backgroundImage = ''; //сброс фонового изображения
+			}
+			if (dots[index].classList.contains("active")) {
+				dots[index].classList.remove("active");
+			}
+		})
+	}
+
+	slides.forEach((slide, i) => {
+		dots[i].addEventListener("click", () => {
+			slidesResetActive();
+			slide.classList.add("active");
+			slide.style.backgroundImage = `url('./slide-${i}.png')`;
+			dots[i].classList.add("active")
+		});
+	});
 }
 
-function currentSlide(sliderNum, n) {  // Функция для переключения на указанный слайд
-  showSlides(sliderNum, n);
-}
-
-document.addEventListener('DOMContentLoaded', function() { 
-  showSlides(1, 0);     // Отобразить первый слайд при загрузке страницы
+document.addEventListener('DOMContentLoaded', function () {
+	initSlider();
 });
